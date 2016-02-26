f9a0febb6edcc7220c1b83d57ca9893dc31f6bfb
Mohammed Khalilia <mkhali8@smtp.service.emory.edu>
2016-02-26 10:21:37 -0500

    Add comments

--- adrc-python/web-service/main.py
+++ adrc-python/web-service/main.py
@@ -3,10 +3,14 @@ from routes.slides import slides
 from  flask import Flask
 from utils.config import get_app_configurations
 
+#start the flask app
 app = Flask('dsa_adrc')
+
+#attach configurations stored in app.cfg to the app
 app.config.from_envvar('DEEPZOOM_MULTISERVER_SETTINGS', silent=True)
 app.config.update(get_app_configurations())
 
+#register the route blueprints to the app
 app.register_blueprint(dz)
 app.register_blueprint(slides)
 
