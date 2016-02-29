fb073006e57a18db644f6b6e180fa72155e1458a
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-02-28 21:21:54 -0500

    Fixed the static path problem, closes #1

--- adrc-python/web-service/routes/ADRC_static.py
+++ adrc-python/web-service/routes/ADRC_static.py
@@ -1,14 +1,27 @@
+#!/usr/bin/python
+# Serve static pages
+# Serve pages from path specified in the app configutation 
+# file
+# The path should relative to the location of this blueprint
+
 from flask import Blueprint
 from utils.crossdomains import crossdomain
 from utils.config import get_app_configurations
 
+#get app configurations
 config = get_app_configurations()
-adrc = Blueprint('ADRC_Routes', __name__, static_folder=config['static_dir'])
+adrc = Blueprint('ADRC_Routes', __name__, static_folder = config['static_dir'])
 
 @adrc.route('/')
-def index():
-  return adrc.send_static_file('index.html')
+def serve_index():
+	"""
+	The default request will serve index.html page
+	"""
+	return adrc.send_static_file('index.html')
 
 @adrc.route('/<path:path>')
-def send_static_page(path):
-  return adrc.send_static_file(path)
+def serve_static_page(path):
+	"""
+	Serve the page specified in the path
+	"""
+	return adrc.send_static_file(path)
--- adrc-python/web-service/routes/deepzoom.py
+++ adrc-python/web-service/routes/deepzoom.py
@@ -9,11 +9,18 @@ dz = Blueprint('deepzoom', __name__)
 
 @dz.record
 def record_params(setup_state):
+    """
+    Apply the main app configurations to the blueprint
+    """
     app = setup_state.app
     dz.config = app.config
 
 @dz.before_app_first_request
 def _setup():
+    """
+    Initialize openslide with some parmeters defined in the application
+    configuration file
+    """
     opts = {
        'tile_size': int(dz.config['tile_size']),
        'overlap': int(dz.config['overlap']),
@@ -25,6 +32,10 @@ def _setup():
 @dz.route('/DZIMS/<path:path>.dzi')
 @crossdomain(origin='*')
 def dzi(path):
+    """
+    Service while slide images using openslide
+    images are encolded in XML format
+    """
     slide = _get_slide(dz.config['slides_dir'], path)
     resp = make_response(slide.get_dzi(dz.config['deepzoom_format']))
     resp.mimetype = 'application/xml'
@@ -35,17 +46,13 @@ def tile(path, level, col, row, format):
     slide = _get_slide(dz.config['slides_dir'], path)
     format = format.lower()
     if format != 'jpeg' and format != 'png':
-        # Not supported by Deep Zoom
         abort(404)
     try:
         tile = slide.get_tile(level, (col, row))
     except ValueError:
-        # Invalid level or coordinates
         abort(404)
     buf = PILBytesIO()
  
-
-#   tile.save(buf, format, quality=app.config['DEEPZOOM_TILE_QUALITY'])
     tile.save(buf, 'jpeg', quality=90)
     resp = make_response(buf.getvalue())
     resp.mimetype = 'image/%s' % format
