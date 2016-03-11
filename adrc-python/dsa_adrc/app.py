2dce967df315cee9ad554671f2a81c8f054ff552
David Gutman <dagutman@gmail.com>
2016-03-10 22:53:30 -0500

    cleaning up old non relevant code in main app

--- adrc-python/dsa_adrc/app.py
+++ adrc-python/dsa_adrc/app.py
@@ -10,12 +10,13 @@ import gridfs
 
 import pymongo
 client = pymongo.MongoClient('localhost',27017)
-slide_db_ptr = client['ADRC_Slide_DB']
+slide_db_ptr = client['ADRC_DSA_Slide_DB']
 load_errors_db = client['ADRC_LoadErrors']
 
 app = Flask('dsa_adrc')
-
-app.config['SLIDE_DIR'] = '/mnt/GAUSS_SCRATCH/NDPI_VAULT/ADRC'
+dsa_slide_db = client['ADRC_DSA_Slide_DB']
+#app.config['SLIDE_DIR'] = '/mnt/GAUSS_SCRATCH/NDPI_VAULT/ADRC'
+app.config['SLIDE_DIR'] = '/GLOBAL_SCRATCH/ADRC'
 
 from collections import OrderedDict
 from flask import Flask, abort, make_response, render_template, url_for
@@ -47,7 +48,7 @@ def root():
 @app.route('/api/v1/collections')
 @crossdomain(origin='*')
 def get_collections():
-    coll_list = slide_db_ptr.collection_names(False)
+    coll_list = dsa_slide_db['DSA_Slide_Data'].distinct('pt_id')
     return jsonify( { 'Collections': sorted(coll_list) })
 
 @app.route('/api/v1/collections/slides/<string:coll_name>')
@@ -56,7 +57,7 @@ def get_slides( coll_name):
     """This will return the list of slides for a given collection aka tumor type """
     #return jsonify( {'slide_list':dsa_combined_db['KIRP'].find_one() })
     #return dumps( {'slide_list': dsa_combined_db['KIRP'].find().limit(25) })
-    return dumps( {'slide_list': dsa_combined_db[coll_name.upper()].find() })
+    return dumps( {'slide_list': dsa_slide_db['DSA_Slide_Data'].find({'pt_id': coll_name }) })
 
 
 ##This will process and store files that were marked as bad...
@@ -68,86 +69,6 @@ def report_bad_images():
     load_errors_db['cdsa_live'].insert({ 'filename':filename, 'slide_url':slide_url, 'data_group':data_group})
     return 'OK'
 
-@app.route('/api/v1/path_reports')
-def get_pdf_reports():
-    patientId = 'TCGA-OR-A5J1'
-    matched_files = client['TCGA']['tcgaTarExtractFileList'].find_one( {'patientId': patientId})    
-    ### Eventually I should parse out the logic and see if there's more than one
-    #gridfs_fileId = matched_files['fileId']
-    
-    pdf_fp = pdfGridFs.get( matched_files['fileId'])
-#    def get_pdf(id=None):
-#    if id is not None:
-#    binary_pdf = get_binary_pdf_data_from_database(id=id)
-    binary_pdf = pdf_fp
-    response = make_response(binary_pdf.read())
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = \
-        'inline; filename=%s.pdf' % 'samplegutman.pdf'
-    return response
-
-
-@app.route('/api/v1/path_reports/<string:patientId>')
-def get_patient_pdf_report(patientId):
-    matched_files = client['TCGA']['tcgaTarExtractFileList'].find_one( {'patientId': patientId})    
-    ### Eventually I should parse out the logic and see if there's more than one
-    #gridfs_fileId = matched_files['fileId']
-    
-    pdf_filename = matched_files['filename']
-    pdf_fp = pdfGridFs.get( matched_files['fileId'])
-#    def get_pdf(id=None):
-#    if id is not None:
-#    binary_pdf = get_binary_pdf_data_from_database(id=id)
-    binary_pdf = pdf_fp
-    response = make_response(binary_pdf.read())
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = \
-        'inline; filename=%s.pdf' % pdf_filename
-    return response
-
-
-@app.route('/api/v1/clin_datasources/<string:tumor_type>')
-def get_clin_data_sources(tumor_type):
-    #matched_files = client['TCGA']['tcgaTarExtractFileList'].find_one( {'patientId': patientId})    
-    ### Eventually I should parse out the logic and see if there's more than one
-    #gridfs_fileId = matched_files['fileId']
-    
-    avail_coll_for_tt = [ x for x in biotab_db.collection_names(False) if tumor_type in x]
-    #print avail_coll_for_tt
-    filtered_data_sources = [ x for x in avail_coll_for_tt if 'analyte' not in x] 
-    ### As a secondary check, I am going to see if the bcr_patient_barcode is in the file
-    filtered_data_sources = []
-    for ac in avail_coll_for_tt:
-        pt_key_status = biotab_db[ac].find_one( {'bcr_patient_barcode': {'$exists': True}}  ) 
-        if pt_key_status:
-           filtered_data_sources.append(ac)
-    
-  
-    return jsonify ( {'Clin_Data_Sources': avail_coll_for_tt,  'Filtered_Source_List': filtered_data_sources })
-
-
-
-@app.route('/api/v1/clin_datasources/<string:csv_datasrc>/<string:pid>')
-def get_pt_data_for_data_sources(csv_datasrc,pid):
-    """Given a data source file and a patient id returns the erlevant data as key value pairs"""
-    cur_pt_data = {}
-    #cur_pt_data[csv_datasrc] = {}
-    ### There are sample barcodes and patient barcodes... may need to separate these in the database
-
-    dbp =  biotab_db[csv_datasrc].find_one({'bcr_patient_barcode': pid})
-    #dbp =  biotab_db[csv_datasrc].find_one({'bcr_sample_barcode': pid})
-    if dbp:
-        dbp_keyval_pairs = []
-        for k,v in dbp.iteritems():
-            dbp_keyval_pairs.append([k,v])
-    else:
-        dbp_keyval_pairs = [] 
-
-    #cur_pt_data[csv_datasrc] = dbp_keyval_pairs;
-
-    #return jsonify ( {'data':  cur_pt_data })
-    return dumps( {'data': dbp_keyval_pairs } )
-
 
 
 @app.route('/static/<path:path>')
@@ -155,19 +76,33 @@ def static_proxy(path):
   # send_static_file will guess the correct MIME type
   return app.send_static_file(os.path.join('.', path))
 
-
-
-
-
 @app.route('/<path:path>')
 def static_file(path):
     return app.send_static_file(path)
 
 
+@app.route('/thumbnail/<path:path>')
+@crossdomain(origin='*')
+def getThumbnail(path):
+    """This will return the 0/0 tile later whch in the case of an SVS image is actually the thumbnail..... """
+    print "Looking in ",path,'for thumbnail.... which sould be expanded  I hope'
 
-
-
-
+    slide = _get_slide(path)
+    format = 'jpeg'
+    level=0
+    col=0
+    row=0 
+    ## I believe this is the defaults for level 0.
+    format = format.lower()
+    if format != 'jpeg' and format != 'png':
+        # Not supported by Deep Zoom
+        abort(404)
+    try:
+        tile = slide.get_tile(level, (col, row))
+    except ValueError:
+        # Invalid level or coordinates
+        abort(404)
+    buf = PILBytesIO()
 
 @app.route('/DZIMS/<path:path>.dzi')
 @crossdomain(origin='*')
@@ -200,9 +135,6 @@ def tile(path, level, col, row, format):
     resp = make_response(buf.getvalue())
     resp.mimetype = 'image/%s' % format
     return resp
-
-
-
 
 
 
@@ -243,9 +175,6 @@ class _SlideCache(object):
                 self._cache[path] = slide
         return slide
 
-
-
-
 class _SlideFile(object):
     def __init__(self, relpath):
         self.name = os.path.basename(relpath)
@@ -255,16 +184,12 @@ class _SlideFile(object):
 @app.before_first_request
 def _setup():
     #app.basedir = os.path.abspath(app.config['SLIDE_DIR'])
-    app.basedir = '/mnt/GAUSS_SCRATCH/NDPI_VAULT/ADRC'
-   
+    #app.basedir = '/mnt/GAUSS_SCRATCH/NDPI_VAULT/ADRC'
+    app.basedir = app.config['SLIDE_DIR']
     config_map = {
         'DEEPZOOM_TILE_SIZE': 'tile_size',
         'DEEPZOOM_OVERLAP': 'overlap',
         'DEEPZOOM_LIMIT_BOUNDS': 'limit_bounds',
-        	
-
-
-	
     }
     opts = {
 	'tile_size': 256,
@@ -277,11 +202,9 @@ def _setup():
     app.config['SLIDE_CACHE_SIZE']  = 1000
     app.cache = _SlideCache(app.config['SLIDE_CACHE_SIZE'], opts)
 
-
 def _get_slide(path):
     path = os.path.abspath(os.path.join(app.basedir, path))
     print path,"Is where I am looking";
-
 
     if not path.startswith(app.basedir + os.path.sep):
         # Directory traversal
