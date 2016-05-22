5c95e16651b8c880baeb822416a180e605309546
David Gutman <dagutman@gmail.com>
2016-05-22 16:16:47 -0400

    cleaning up windows

--- static/js/dsa_main.js
+++ static/js/dsa_main.js
@@ -5,9 +5,7 @@
     function handleResize() {
 
         console.log('resize');
-
         // this is expensive, easier to just save these long term, but oh well.
-
         var nav_height = $(".navbar").height();
         var status_bar_height = $('#status_bar').height();
         var select_patient_height = $("#sel_patient").height();
@@ -25,47 +23,7 @@
     window.onresize = handleResize;
 
 
- function load_expanded_thumbnail_data(name) {
-
-        //This is a slightly different endpoint as instead of loading a column of data, I am loading 5 columns of data at a time...
-        //but basic functionality is similar to above
-        //all this is doing is basically reformatting the data for datatables
-
-
-        recent_study_name = name;
-        var html_for_expanded_dt = [];
-        //This actually doesn't require me to load any data...
-        //now want to load the data into the data table object
-        //Now I need to load 5 slides per row, instead of 1
-        if (!thumbs_expanded.hasOwnProperty(name)) {
-            thumbs_expanded[name] = [];
-            console.log(thumbs[name]);
-            for (var i = 0; i <= thumbs[name].length - thumbs[name].length % 5; i += 5) {
-                if (i == thumbs[name].length) break; //may need to make sure I don't need to push whatever is in the current row
-                var row = [];
-                for (var j = 0; j < 5 && ((i + j) < thumbs[name].length); j++) {
-
-                    //Each row actually has the following info
-                    var cur_html = thumbs[name][i + j].slide_name + '#' + thumbs[name][i + j].slide_url + '#' + thumbs[name][i + j].slide_name + '#' + thumbs[name][i + j].thumbnail_image;
-
-                    row.push(cur_html);
-                    //console.log(thumbs[name[i+j]]);
-                }
-                while (row.length < 5) {
-                    row.push("");
-                }
-
-                thumbs_expanded[name].push(row);
-                console.log(row);
-            }
-        }
-
-        //console.log(html);
-
-        load_slides_into_datatable(thumbs_expanded[name], 5);
-    }
-
-
+ 
 
  function load_expanded_thumbnail_data(name) {
 
@@ -135,43 +93,17 @@
 
     }
 
-        //Below formats the data for either a 1 or 5 column data view
-        //There are also separate call back functions for the single and 5 column viewer... but at least I consolidated
-        // the function calls
-        
-
-
- 
+  
 
     function load_image(filename, image_url) {
-
-        if (sel_image_expanded) {
-            $('#sel_image_frame').addClass('span3');
-            $('#sel_image_frame').removeClass('span12');
-
-            $('#zoom_frame').show();
-
-            load_thumbnail_data(recent_study_name);
-            sel_image_expanded = false;
-        }
 
         image_url = base_host + image_url;
         //annotationState.clearAnnotations();
         viewer.open(image_url);
-
-
         //Once an image is selected, buttons become clickable depending on the data source
-        $('#show_annotator').removeAttr('disabled');
-        $("#show_metadata").removeAttr('disabled');
-        $('#show_filter').removeAttr('disabled');
         current_filename = filename;
         current_slide_url = image_url;
-        $('#report_bad_image_btn').removeAttr('disabled');
      	$("#status_bar").text("Current image:"+current_filename); //update status bar to show current image name
-
-
-
-
     }
 
 
@@ -209,7 +141,6 @@
             results = regex.exec(location.search);
         return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
-
 
 $(function() {
   // initialize the image viewer and annotation state
@@ -255,12 +186,7 @@ $(function() {
             }
         });
             $('#data_group').change(function() {
-            if (!sel_image_expanded) {
-                //load_thumbs($("#data_group option:selected").val());
-                load_thumbnail_data($("#data_group option:selected").val());
-            } else {
-                load_expanded_thumbnail_data($("#data_group option:selected").val());
-            }
+                load_thumbnail_data($("#data_group option:selected").val());          
         });
      
 });
@@ -280,35 +206,8 @@ $(function() {
                 "sType": "html"
             }];
             callback_to_use = customFnRowCallback;
-        } else if (images_per_row == 5) {
-            aoColumns = [{
-                "sTitle": "Image",
-                "sClass": "center",
-                "sType": "html"
-            }, {
-                "sTitle": "Image",
-                "sClass": "center",
-                "sType": "html"
-            }, {
-                "sTitle": "Image",
-                "sClass": "center",
-                "sType": "html"
-            }, {
-                "sTitle": "Image",
-                "sClass": "center",
-                "sType": "html"
-            }, {
-                "sTitle": "Image",
-                "sClass": "center",
-                "sType": "html"
-            }]
-
-            callback_to_use = customFnRowCallback_expanded;
-
+        
         }
-       // console.log(aoColumns);
-
-
 
         ///I am currently using data tables for the thumbnail browser on the left, this code below loads it
         $('#count_patient').text(html_for_dataTable.length);
