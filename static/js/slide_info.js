ec7c8ad523ed469a726c1896490fc2327e124f87
Mohammed Khalilia <mohammed@Mohammeds-MacBook-Pro.local>
2016-06-05 08:22:59 -0400

    text field added for slide name

--- static/js/slide_info.js
+++ static/js/slide_info.js
@@ -1,12 +1,21 @@
 var SlideInfoView = Backbone.View.extend({
 	el: "#slide_info",
 
+	events: {
+		"blur #new_slide_name": "updateslidename"
+	},
+
 	initialize: function(){
-		console.log(this.model.defaults);
+		this.model.on("change", this.render, this);
 		this.render();
 	},
 
 	render: function(){
 		this.$el.html(JST["slide_info"](this.model.toJSON()));
+		return this;
+	},
+
+	updateslidename: function(){
+		this.model.set("slide_name", $("#new_slide_name").val());
 	}
 });
