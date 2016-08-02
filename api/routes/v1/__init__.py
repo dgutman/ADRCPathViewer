6c657ac7f948315f28a5d2a17a4fd8f12e083e52
Mohammed Khalilia <mohammed@Mohammeds-MacBook-Pro.local>
2016-08-02 16:19:52 -0400

    Inline comments for routes/vi/__init__.py

--- api/routes/v1/__init__.py
+++ api/routes/v1/__init__.py
@@ -6,10 +6,14 @@ from DeepZoom import DeepZoom
 from Thumbnail import Thumbnail
 from Slide import Slide
 
+# Create a blueprint for API v1
+# It is prefixed with /v1
+# Add CORS decorator for all endpoints
+# Attach all endpoints to the v1 API
 v1 = Blueprint('v1', __name__)
 api = Api(v1, prefix="/v1")
 api.decorators=[cors.crossdomain(origin='*')]
-api.add_resource(SlideSet, "/slideset/<string:id>")
-api.add_resource(DeepZoom, "/deepzoom/<path:path>.dzi")
-api.add_resource(Thumbnail, "/thumbnail/<path:path>")
-api.add_resource(Slide, "/slide/<string:id>")
+api.add_resource(SlideSet, "/slideset/<string:id>", endpoint="slideset")
+api.add_resource(DeepZoom, "/deepzoom/<path:path>.dzi", endpoint="deepzoom")
+api.add_resource(Thumbnail, "/thumbnail/<path:path>", endpoint="thumbnail")
+api.add_resource(Slide, "/slide/<string:id>", endpoint="slide")
--- api/start_gunicorn.sh
+++
@@ -1,1 +0,0 @@
-gunicorn --bind='0.0.0.0:5080' -w 6 --reload app:app
