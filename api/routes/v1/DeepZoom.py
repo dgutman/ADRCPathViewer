8024d575a7ebe19739277c6f398f8102cdf7386c
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-08-03 15:53:43 -0400

    Adding DeepZoom endpoint

--- api/routes/v1/DeepZoom.py
+++ api/routes/v1/DeepZoom.py
@@ -1,11 +1,18 @@
 from flask_restful import Resource
+from flask import make_response
 from utils.config import get_app_configurations
 from utils.cache import cache
+from utils.deepzoom import _get_slide, _SlideCache
 
 class DeepZoom(Resource):
     def __init__(self):
         self.config = get_app_configurations()
 
     @cache.cached()	
-    def get(self, path):
-        return {'endpoint': 'return deep zoom image ' + path}
+    def get(self, id):
+    	image = self.slides.find_one({'_id': ObjectId(id)})
+    	path = image["group"] + "/" + image["filename"]
+		slide = _get_slide(self.config['slides_dir'], path)
+		resp = make_response(slide.get_dzi(self.config['deepzoom_format']))
+		resp.mimetype = 'application/xml'
+		return resp
