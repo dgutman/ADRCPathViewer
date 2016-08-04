3ace80003f231ce710a5df2b45731702bcb1dd90
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-08-04 14:22:30 -0400

    Error handling in DeepZoom

--- api/routes/v1/DeepZoom.py
+++ api/routes/v1/DeepZoom.py
@@ -1,6 +1,6 @@
 import os, openslide
 from flask_restful import Resource
-from flask import make_response
+from flask import make_response, Response
 from bson.objectid import ObjectId
 from openslide import OpenSlide
 from openslide.deepzoom import DeepZoomGenerator
@@ -19,6 +19,8 @@ class DeepZoom(Resource):
 		image = self.slides.find_one({'_id': ObjectId(id)})
 		path = os.path.join(self.config["slides_dir"], image["group"], image["filename"])
 		slide = get_slide(self.config['slides_dir'], path)
-		resp = make_response(slide.get_dzi(self.config['deepzoom_format']))
-		resp.mimetype = 'application/xml'
-		return resp
+
+		if slide == None:
+			Response("", status=400, mimetype='application/xml')
+		else:
+			return Response(slide.get_dzi(self.config['deepzoom_format']), status=200, mimetype='application/xml')
