1311dc2449ad96300dd4897ed1e93b87d747ab48
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-09-09 19:22:44 +0000

    changed gunciron workers to 16

--- api/dsa.sh
+++ api/dsa.sh
@@ -6,7 +6,7 @@ source /home/dgutman/osdTester/bin/activate
 export PYTHONPATH=/home/dgutman/osdTester:$PYTHONPATH
 
 exec gunicorn -b 0.0.0.0:5080 \
-	--workers=65 \
+	--workers=16 \
 	--log-level debug \
 	--name dsa \
 	--chdir /home/mkhali8/dev/ADRCPathViewer/api wsgi
