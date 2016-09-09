b91507b8a08d205992208d15cf1d56c0d3d13b10
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-09-09 15:09:51 +0000

    Override slideset list in app configuration file

--- static/app/config.js
+++ static/app/config.js
@@ -5,7 +5,7 @@ define("config", function(){
 	//SLIDE_SETS will tell the app which slidesets to make available for the user
 	//Options 1: All slidesets:
 	//  SLIDE_SETS = BASE_URL +  "/slidesetlist" which gets all slidesets
-	//Options 2: specifoc slidesets
+	//Options 2: specific slidesets
 	//  SLIDE_SET = ["WINSHIP_BIOBANK"]
 	var SLIDE_SETS = BASE_URL +  "/slidesetlist";
 
