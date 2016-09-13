b5e60f998a801cb3e9df92a44acea38394a418d2
David Gutman <dagutman@gmail.com>
2016-09-13 08:34:42 -0400

    slidelist not working

--- static/app/app-slidelist.js
+++ static/app/app-slidelist.js
@@ -1,36 +1,40 @@
-define(["jquery","webix"], function($) {
+define(["jquery", "webix"], function($) {
 
+    function sldtClicked(id) {
+        console.log("The slide data table was clicked at" + id);
+        itemData = $$("slidelist").getItem(id);
+        console.log(itemData);
+        $("#imgThumb").attr("src", apiBase + '/v1/thumbnail/' + id);
 
-    function sldtClicked(id)
-        {
-            console.log("The slide data table was clicked at"+id);
-            itemData = $$("slidelist").getItem(id);
-            console.log(itemData);
-            $("#imgThumb").attr("src", apiBase+'/v1/thumbnail/' + id);
-            $("#imgMacro").attr("src", apiBase+'/v1/macroimage/' + id);
-            $("#imgLabel").attr("src", apiBase+'/v1/labelimage/' + id);
-        }
+        //The below links only work if Logged in..
+        // $("#imgMacro").attr("src", apiBase + '/v1/macroimage/' + id);
+        // $("#imgLabel").attr("src", apiBase + '/v1/labelimage/' + id);
+    }
 
     apiBase = 'http://digitalslidearchive.emory.edu:5080';
 
-        header = {borderless: true, cols: [{
-            view:"toolbar", height:66, css: "toolbar",
-                cols:[
-                    {view: "template", borderless:true, template: "<img src='img/CDSA_Slide_50.png' height='40'/>", width: 200},
-                    {view: "template", template: "<h1>DSA UberSlide Sorter</h1>", borderless:true},
-                    {},
-                    {view: "template", borderless:true, template: "<img src='http://cancer.digitalslidearchive.net/imgs/Winship_06-2011/Winship_NCI_shortTag/horizontal/jpg_png/Winship_NCI_shortTag_hz_280.png' height='50'/>", width: 160},
-                ]
-            }]
-        };
-        ///I may want to add back in the menu instead of {}----  
+    header = {
+        borderless: true,
+        cols: [{
+            view: "toolbar",
+            height: 66,
+            css: "toolbar",
+            cols: [
+                { view: "template", borderless: true, template: "<img src='img/CDSA_Slide_50.png' height='40'/>", width: 200 },
+                { view: "template", template: "<h1>DSA UberSlide Sorter</h1>", borderless: true },
+                {},
+                { view: "template", borderless: true, template: "<img src='http://cancer.digitalslidearchive.net/imgs/Winship_06-2011/Winship_NCI_shortTag/horizontal/jpg_png/Winship_NCI_shortTag_hz_280.png' height='50'/>", width: 160 },
+            ]
+        }]
+    };
+    ///I may want to add back in the menu instead of {}----  
 
     //Top LEvel will have three panels
     //Top is a View Bar
     //with a left panel for info and a bigger panel that allows us to switch views between a tab
     //and data table view
     slideListDataTable_Columns = [
-        { id: "thumbnail", header: "Thumbnail", width: 100, template: "<img src='" + apiBase +"/v1/thumbnail/#id#' height=30 width=80/>" },
+        { id: "thumbnail", header: "Thumbnail", width: 100, template: "<img src='" + apiBase + "/v1/thumbnail/#id#' height='40' width='80'/>" },
         { id: "fileName", header: ["File Name", { content: "serverFilter" }], width: 300 },
         { id: "slideSet", header: ["Slide Set", { content: "serverSelectFilter" }], width: 200 },
         { id: "width", sort: "server", header: "Width", width: 80 },
@@ -42,41 +46,53 @@ define(["jquery","webix"], function($) {
     ]
 
     wbx_SlideListDataTable = {
-        id: "slidelist",
-        view: "datatable",
-        columns: slideListDataTable_Columns,
-        url: apiBase+ "/v1/slides",
-        select: true,
-        navigation: true,
-        pager: {
+        rows: [  { cols: [{
+            view: "pager",
             template: "{common.first()} {common.prev()} {common.pages()} {common.next()} {common.last()} (Total Slides #count#)",
             container: "paging",
             size: 20,
-            group: 5
-        },
-        datafetch: 20,
-        loadahead: 20,
-        on: { "onItemClick": sldtClicked },        
+            group: 5,
+            id: "gridPager"
+        }, {view:"template", template:"Template1 Template 2"}]}, 
+
+        {
+            id: "slidelist",
+            view: "datatable",
+            columns: slideListDataTable_Columns,
+            url: apiBase + "/v1/slides",
+            select: true,
+            navigation: true,
+            pager: "gridPager",
+            datafetch: 20,
+            loadahead: 20,
+            on: { "onItemClick": sldtClicked },
+        }]
     }
 
 
     wbx_SlideDataView = {
-    view:"dataview",
-    select: true,
+        rows: [
 
-    template:"<img src='"+apiBase+"/v1/thumbnail/#id#' ><div class='webix_strong'>#fileName#</div> Size: #width# x #height#",
-    type:{
-        height:150
-    },
-    url: apiBase+ "/v1/slides",
-     pager: {
-            template: "{common.first()} {common.prev()} {common.pages()} {common.next()} {common.last()} (Total Slides #count#)",
-            
-            size: 20,
-            group: 5
-        },
-        datafetch: 20,
-        loadahead: 20,
+            {
+                view: "pager",
+                id: "pagerA",
+                size: 50,
+                group: 5,
+                template: "{common.first()} {common.prev()} {common.pages()} {common.next()} {common.last()} (Total Slides #count#)",
+                datafetch: 50,
+                loadahead: 50
+            }, {
+                view: "dataview",
+                select: true,
+
+                template: "<img src='" + apiBase + "/v1/thumbnail/#id#'  ><div class='webix_strong'>#fileName#</div>Size: #width# x #height#",
+                type: {
+                    height: 150
+                },
+                url: apiBase + "/v1/slides",
+                pager: "pagerA"
+            }
+        ]
     }
 
 
@@ -85,6 +101,7 @@ define(["jquery","webix"], function($) {
     mainDataPanel = {
         view: "tabview",
         id: "wbxLeftPanel",
+
         cells: [
 
             { header: "GridView", body: { rows: [wbx_SlideListDataTable] } },
@@ -92,12 +109,10 @@ define(["jquery","webix"], function($) {
             {
                 header: "DataTableView",
                 body: {
-
-                    rows: [{ template: "Controls could go here" }, { view: 'resizer' },  wbx_SlideDataView]
+                    rows: [{ template: "Controls could go here", height: 20 }, { view: 'resizer' }, wbx_SlideDataView]
                 }
             },
             { header: "About", body: { id: "about", content: "about_text", } },
-            
         ],
         tabbar: {
             on: {
@@ -108,17 +123,18 @@ define(["jquery","webix"], function($) {
         }
     };
 
-    metaImageViewer = { view: "template", "content": "dv_imageReview", height: 200 };
+    metaImageViewer = { view: "template", "content": "dv_imageReview", width: 200 };
 
     RawSlideLayout = {
         container: "main_layout",
+
         id: "mylayout",
         type: "space",
         rows: [
             header, //here you place any component you like
-            metaImageViewer,
-            { view: "resizer"},
-            mainDataPanel
+
+            { view: "resizer" },
+            { cols: [metaImageViewer, mainDataPanel], fillspace: true }
         ]
     }
 
