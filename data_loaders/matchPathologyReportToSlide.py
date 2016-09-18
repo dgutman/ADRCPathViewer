89484c07a142d8cc88b419f37c7893e23e93bfd4
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-09-18 00:40:09 +0000

    match pathology reports to slide

--- data_loaders/matchPathologyReportToSlide.py
+++ data_loaders/matchPathologyReportToSlide.py
@@ -10,22 +10,22 @@ db = client[DB_NAME][DB_COLL]
 p = re.compile('^(\d+)_.*\.pdf$', re.IGNORECASE)
 slides = {}
 
-for root, dirs, files in os.walk(PDF_DIR):
-	for subroot, sudirs, filenames in os.walk(root):
-		for filename in filenames:
-			m = p.match(filename)
+for root, dirs, filenames in os.walk(PDF_DIR):
+	#for subroot, sudirs, filenames in os.walk(root):
+	for filename in filenames:
+		m = p.match(filename)
 
-			if m != None:
-				searchTerm = "EUH_" + m.groups(0)[0]
-				absFilePath = os.path.join(subroot, filename)
+		if m != None:
+			searchTerm = "EUH_" + m.groups(0)[0]
+			absFilePath = os.path.join(root, filename)
 	            
-				slide = db.find_one({"fileName": {"$regex": searchTerm}}, {"scanProperties": False})
+			slide = db.find_one({"fileName": {"$regex": searchTerm}}, {"scanProperties": False})
 
-				if slide:
-					slideId = str(slide["_id"])
-					if slideId not in slides: slides[slideId] = []
+			if slide:
+				slideId = str(slide["_id"])
+				if slideId not in slides: slides[slideId] = []
 
-					slides[slideId].append({"fileName": filename, "filePath": absFilePath})
+				slides[slideId].append({"fileName": filename, "filePath": absFilePath})
 
 for slideId, files in slides.iteritems():
 	print slideId
