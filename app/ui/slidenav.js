00d777e844f280c944ce0415fb8323875c5ab1e7
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-10-14 21:01:24 +0000

    bug fixes in the thumbnail viewer

--- app/ui/slidenav.js
+++ app/ui/slidenav.js
@@ -15,10 +15,6 @@ define("ui/slidenav", ["config", "zoomer", "slide", "jquery", "aperio", "request
             height: 170,
             width: 200
         },
-        ready: function() {
-            item = this.getItem(this.getFirstId());
-            slide.init(item);
-        },
         on: {
             "onItemClick": function(id, e, node) {
                 item = this.getItem(id);
@@ -40,31 +36,26 @@ define("ui/slidenav", ["config", "zoomer", "slide", "jquery", "aperio", "request
         },
         on: {
             "onChange": function(id) {
+                $$("thumbnail_search").setValue("");
                 var item = this.getPopup().getBody().getItem(id);
-                var samples = $$("samples_list").getPopup().getList();
-                var url = config.BASE_URL + "/folder?parentType=folder&parentId=" + item._id;
-                samples.clearAll();
-                samples.load(url);
+                
+                $.get(config.BASE_URL + "/folder?parentType=folder&parentId=" + item._id, function(data){
+                    var sFoldersMenu = $$("samples_list").getPopup().getList();
+                    sFoldersMenu.clearAll();
+                    sFoldersMenu.parse(data);
+                    $$("samples_list").setValue(data[0].id);
+                });
             },
             "onAfterRender": function() {
-                $.ajax({
-                    url: config.BASE_URL + "/resource/lookup?path=/collection/" + config.COLLECTION_NAME,
-                    method: "GET"
-                }).then(function(collection){
-                    return $.ajax({
-                        url: config.BASE_URL + "/folder?parentType=collection&parentId=" + collection._id,
-                        method: "GET"
-                    });
+                $.get(config.BASE_URL + "/resource/lookup?path=/collection/" + config.COLLECTION_NAME)
+                 .then(function(collection){
+                    return $.get(config.BASE_URL + "/folder?parentType=collection&parentId=" + collection._id);
                 }).then(function(folders){
                     var foldersMenu = $$("slideset_list").getPopup().getList();
                     foldersMenu.clearAll();
                     foldersMenu.parse(folders);
                     $$("slideset_list").setValue(folders[0].id);
-
-                    return $.ajax({
-                        url: config.BASE_URL + "/folder?parentType=folder&parentId=" + folders[0]._id,
-                        method: "GET"
-                    });
+                    return $.get(config.BASE_URL + "/folder?parentType=folder&parentId=" + folders[0]._id);
                 }).done(function(data){
                     var sFoldersMenu = $$("samples_list").getPopup().getList();
                     sFoldersMenu.clearAll();
@@ -88,11 +79,14 @@ define("ui/slidenav", ["config", "zoomer", "slide", "jquery", "aperio", "request
         },
         on: {
             "onChange": function(id) {
-                var item = this.getPopup().getBody().getItem(id);
-                var thumbs = $$("thumbnails_panel");
-                var url = config.BASE_URL + "/item?folderId=" + item._id;
-                thumbs.clearAll();
-                thumbs.load(url);
+                $$("thumbnail_search").setValue("");
+                if(id){
+                    var item = this.getPopup().getBody().getItem(id);
+                    var thumbs = $$("thumbnails_panel");
+                    var url = config.BASE_URL + "/item?folderId=" + item._id;
+                    thumbs.clearAll();
+                    thumbs.load(url);
+                }
             }
         }
     };
@@ -105,17 +99,38 @@ define("ui/slidenav", ["config", "zoomer", "slide", "jquery", "aperio", "request
         group: 4
     };
 
+     filter = {
+        view: "search",
+        id: "thumbnail_search",
+        placeholder: "Search",
+        on: {"onChange": searchItems}
+    };
+
     //slides panel is the left panel, contains two rows 
     //containing the slide group dropdown and the thumbnails panel 
     slidesPanel = {
         header: "Slide Controls",
         body: {
             rows: [
-                dropdown, samples_dropdown, thumbPager, thumbnailsPanel
+                dropdown, samples_dropdown, filter, thumbPager, thumbnailsPanel
             ]
         },
         width: 220
     };
+
+    function searchItems(keyword = null){
+        url = config.BASE_URL + "/item?text=" + keyword;
+        folderId = $$("samples_list").getValue();
+
+        if(folderId){
+            item = $$("samples_list").getPopup().getList().getItem(folderId);
+            url = config.BASE_URL + "/item?folderId=" + item._id + "&text=" + keyword;
+        }
+        console.log(url);
+        var thumbs = $$("thumbnails_panel");
+        thumbs.clearAll();
+        thumbs.load(url);
+    }
 
     return {
         view: slidesPanel
