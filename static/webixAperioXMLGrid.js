e0822801d1110c625433d82f16b607dc3b30c0a6
David Gutman <dagutman@gmail.com>
2016-11-11 10:29:51 -0500

    added xml to json

--- static/webixAperioXMLGrid.js
+++ static/webixAperioXMLGrid.js
@@ -41,6 +41,49 @@ autoConfig: true
 
 }
 
+     // aperio_win_grid2.addRow(1, ["Id:", this.getAttribute("Id")], 1);
+     //            aperio_win_grid2.addRow(2, ["Name:", this.getAttribute("Name")], 2);
+     //            aperio_win_grid2.addRow(3, ["ReadOnly:", this.getAttribute("ReadOnly")], 3);
+     //            aperio_win_grid2.addRow(4, ["NameReadOnly:", this.getAttribute("NameReadOnly")], 4);
+     //            aperio_win_grid2.addRow(5, ["LineColorReadOnly:", this.getAttribute("LineColorReadOnly")], 5);
+     //            aperio_win_grid2.addRow(6, ["Incremental:", this.getAttribute("Incremental")], 6);
+     //            aperio_win_grid2.addRow(7, ["Type:", this.getAttribute("Type")], 7);
+     //            aperio_win_grid2.addRow(8, ["LineColor:", this.getAttribute("LineColor")], 8);
+     //            aperio_win_grid2.addRow(9, ["Visible:", this.getAttribute("Visible")], 9);
+     //            aperio_win_grid2.addRow(10, ["Selected:", this.getAttribute("Selected")], 10);
+     //            aperio_win_grid2.addRow(11, ["MarkupImagePath:", this.getAttribute("MarkupImagePath")], 11);
+     //            aperio_win_grid2.addRow(12, ["MacroName:", this.getAttribute("MacroName")], 12);
+     //            // aperio_win_grid2.addRow(2,["LineColor:",this.getAttribute("LineColor")],2);
+     //            linecolor = this.getAttribute("LineColor").toString(16);
+     //            linecolor = (linecolor.length < 6) ? "0" + linecolor : linecolor;
+          
+
+ROIColumns = [ {id:"Id"},{id:"Name"},{id:"ReadOnly"},{id:"NameReadOnly"},{id:"LineColorReadOnly"},
+               {id:"Incremental"}, {id:"Type"}, {id:"LineColor"}, {id:"Visible"},{id:"Selected"},
+               {id:"MarkupImagePath"},{id:"MacroName"}]
+
+layoutROIInfo =
+{
+view: "datatable",
+width: "auto",
+columns: ROIColumns,
+autoConfig: true
+
+
+}
+
+
+
+function aperioLineColortoHex(int) {
+  int = parseInt(int);
+    var part1 = int & 255;
+    var part2 = ((int >> 8) & 255);
+    var part3 = ((int >> 16) & 255);
+   // var part4 = ((int >> 24) & 255);
+   // return part4 + "." + part3 + "." + part2 + "." + part1;
+  return part1.toString(16) + part2.toString(16)+ part3.toString(16);
+  }
+
 
 
 
@@ -52,7 +95,6 @@ webix.ready(function()
 console.log("Window shold pop up??");
 webix.ui({
     view:"window",
-
     id:"winAperioMarkups",
     move: true,
     resize: true,
@@ -71,7 +113,7 @@ webix.ui({
         view: "layout", 
         rows: [
          { cols: [ layoutAnnotationFileList,layoutAnnotationList,layoutParameterList ]},
-        {view: 'template',template:"LeftLower" }
+        layoutROIInfo
          ]
     }
 }).show();
@@ -95,17 +137,66 @@ var layout = webix.ui({
 });
 
 
-// webix.message({
-//     type:"error", 
-//     text:"Form Data is Invalid",
-//     expire:2000
-//     //expire:-1   for canceling expire period
-// })
+aperio_xmlFile = getAperioXML_document(XMLExampleFile);
+
+
+//I am going to turn the aperioXML File into a JSON Object that's WEBIX friendly
+function parseAperioXMLFile( XMLDocument )
+  {
 
 
 
-//https://isic-archive.com/girder#collection/55943cff9fc3c13155bcad61/folder/573f11119fc3c132505c0ee7
+
+  }
+
+// XMLResponse = aperioxml_annotation;
+//         $('Annotation', XMLResponse).each(function() {
+        
+//
+//<Annotation Id="1" Name="tumor" ReadOnly="0" NameReadOnly="0" LineColorReadOnly="0" Incremental="0" Type="4" LineColor="65280" Visible="1" Selected="0" MarkupImagePath="" MacroName="">
 
 })
+
+
+
+function xmlToJson(xml) {
+  
+  // Create the return object
+  var obj = {};
+
+  if (xml.nodeType == 1) { // element
+    // do attributes
+    if (xml.attributes.length > 0) {
+    obj["@attributes"] = {};
+      for (var j = 0; j < xml.attributes.length; j++) {
+        var attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType == 3) { // text
+    obj = xml.nodeValue;
+  }
+
+  // do children
+  if (xml.hasChildNodes()) {
+    for(var i = 0; i < xml.childNodes.length; i++) {
+      var item = xml.childNodes.item(i);
+      var nodeName = item.nodeName;
+      if (typeof(obj[nodeName]) == "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof(obj[nodeName].push) == "undefined") {
+          var old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
+        }
+        obj[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+  return obj;
+};
+
+
 
 
